4facc2d6861d83d58e2153c2a91d130598e00b08
jay <jay@qq.com>
2023-02-02 14:37:41 +0800

    hot-fix commit1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("hot-fix test");
     }
 }
